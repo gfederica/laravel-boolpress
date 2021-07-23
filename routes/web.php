1f3abd1a734c957cf471b202e831e17b257edb7a
gfederica <effegiudice@gmail.com>
2021-07-23 18:52:20 +0200

    Popolato il seeder con gli articoli

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 //rotte per autenticazione
 // Auth::routes(["register" => false]); // per disabilitare la registrazione
 // Auth::routes(["verify" => true]); // per attivare la verifica mail al momento della registrazione // di default è disabilitata
@@ -37,3 +33,5 @@ Route::middleware('auth') // autenticazione
 
 });
 
+// Rotte pubblicke
+Route::get('/', 'HomeController@index')->name('home');
