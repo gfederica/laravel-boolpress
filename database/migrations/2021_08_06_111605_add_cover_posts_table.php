7e6d805da28379303a17166c5fc2d5f31ba3ee22
gfederica <effegiudice@gmail.com>
2021-08-06 13:28:14 +0200

    FileStorage:default public,symlink,migration cover

---
+++ database/migrations/2021_08_06_111605_add_cover_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCoverPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //aggiungiamo nuova colonna cover con vincolo nullable e con after per decidere la posizione fra le colonne
+            $table->string('cover')->nullable()->after('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('cover');
+        });
+    }
+}
